1ec7e3347895b62aa87f7057c772696b85341a18
Magnus Holm <magnusthestrup@hotmail.com>
2022-11-22 09:21:39 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Opens and closes the mobile menu
+function menu() {
+    var menu = document.querySelector("#mobile_overlay");
+    var body = document.querySelector("body");
+    if (menu.style.width == "100%") {
+        menu.style.width = "0";
+        body.style.overflow = "visible";
+    } else {
+        menu.style.width = "100%";
+        body.style.overflow = "hidden";
+    }
+}
